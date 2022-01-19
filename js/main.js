d9686f3a6be17a298c7156b670de5da7901d8170
dan9708 <thdgus0829@gmail.com>
2022-01-19 21:08:39 +0900

    js: 타이머, 게임정지버튼, 다시시작버튼, 사운드추가

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@
 
 // 당근과 벌레를 랜덤으로 배치
 const CARROT_SIZE = 80;
-const CARROT_COUNT = 5;
-const BUG_COUNT = 5
+const CARROT_COUNT = 20;
+const BUG_COUNT = 20;
+const GAME_DURATION_SEC = 20;
 
 const field = document.querySelector('.game__field');
 const fieldRect = field.getBoundingClientRect();
@@ -13,9 +14,22 @@ const gameBtn = document.querySelector('.game__btn');
 const gameTimer = document.querySelector('.game__timer');
 const gameScore = document.querySelector('.game__score');
 
+const popUp = document.querySelector('.pop-up');
+const popUpRefresh = document.querySelector('.pop-up__refresh');
+const popUpText = document.querySelector('.pop-up__message');
+
+const carrotSound = new Audio('../sound/carrot_pull.mp3');
+const alertSound = new Audio('../sound/alert.wav');
+const bgSound = new Audio('../sound/bg.mp3');
+const bugSound = new Audio('../sound/bug_pull.mp3');
+const winSound = new Audio('../sound/game_win.mp3');
+
+
 let started = false;
 let score = 0;
 let timer = undefined;
+
+field.addEventListener('click', onFieldClick);
 
 gameBtn.addEventListener('click', () => {
   if(started) {
@@ -23,24 +37,53 @@ gameBtn.addEventListener('click', () => {
   } else {
     startGame();
   }
-  started = !started;
+})
+
+popUpRefresh.addEventListener('click', () => {
+  startGame();
+  hidePopUp();
 })
 
 function startGame() {
+  started = true;
   initGame();
   showStopButton();
   showTimerAndScore();
   startGameTimer();
+  playSound(bgSound);
 }
 
 function stopGame() {
+  started = false;
+  stopGameTimer();
+  hideGameButton();
+  showPopUpWithText('REPLAY❓');
+  playSound(alertSound);
+  stopSound(bgSound);
+}
 
+function finishGame(win) {
+  started = false;
+  hideGameButton();
+  if(win) {
+    playSound(winSound);
+  } else {
+    playSound(bugSound);
+  }
+  stopGameTimer();
+  stopSound(bgSound);
+  showPopUpWithText(win ? 'YOU WON🎉' : 'YOU LOST💩');
 }
 
 function showStopButton() {
-  const icon = gameBtn.querySelector('.fa-play');
+  const icon = document.querySelector('.fas');
   icon.classList.add('fa-stop');
   icon.classList.remove('fa-play');
+  gameBtn.style.visibility = 'visible';
+}
+
+function hideGameButton() {
+  gameBtn.style.visibility = 'hidden';
 }
 
 function showTimerAndScore() {
@@ -48,12 +91,78 @@ function showTimerAndScore() {
   gameScore.style.visibility = 'visible';
 }
 
+function startGameTimer() {
+  let remainingTimeSec = GAME_DURATION_SEC;
+  updateTimerText(remainingTimeSec);
+  timer = setInterval(() => {
+    if(remainingTimeSec <= 0) {
+      clearInterval(timer);
+      finishGame(CARROT_COUNT === score);
+      return;
+    }
+    updateTimerText(--remainingTimeSec)
+  }, 1000);
+}
+
+function stopGameTimer() {
+  clearInterval(timer);
+}
+
+function updateTimerText(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  gameTimer.innerText = `${minutes}:${seconds}`;
+}
+
+function showPopUpWithText(text) {
+  popUpText.innerText = text;
+  popUp.classList.remove('pop-up--hidden');
+}
+
+function hidePopUp() {
+  popUp.classList.add('pop-up--hidden');
+}
+
 function initGame () {
+  score = 0;
   field.innerHTML = '';
   gameScore.innerText = CARROT_COUNT;// 당근의 갯수 설정
   // 벌레와 당근을 생성한뒤 field에 추가해줌
   addItem('carrot', CARROT_COUNT, '../img/carrot.png');
   addItem('bug', BUG_COUNT, '../img/bug.png');
+}
+
+function onFieldClick(event) {
+  if(!started) {
+    return;
+  }
+  const target = event.target;
+  if(target.matches('.carrot')) {
+    // 당근
+    target.remove();
+    score++;
+    playSound(carrotSound);
+    updateScoreBoard();
+    if(score === CARROT_COUNT) {
+      finishGame(true);
+    }
+  } else if(target.matches('.bug')) {
+    // 벌레
+    finishGame(false);
+  }
+}
+
+function playSound(sound) {
+  sound.currentTime = 0
+  sound.play();
+}
+
+function stopSound(sound) {
+  sound.pause();
+}
+
+function updateScoreBoard() {
+  gameScore.innerText = CARROT_COUNT - score;
 }
 
 function addItem(className, count, imgPath) {
